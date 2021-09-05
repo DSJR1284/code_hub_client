6d63a9d5af23a16892b2a81d3473ee8920a4b1d2
Doug Jones <DSJR1284@GMAIL.COM>
2021-09-05 18:02:36 -0400

    set up docs page

---
+++ src/componets/Docs.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const Docs = () => {
+    return (
+        <div className='docs'>
+            Welcome To Our Docs Page Below You Will Find A ListOf Code Related Doc To Help You On Your Journey
+            <br></br><br></br>
+            <a target="_blank" rel="noreferrer" href='https://ruby-doc.org/'>Ruby</a>
+            <br></br><br></br>
+            <a target="_blank" rel="noreferrer" href='https://guides.rubyonrails.org/'>Ruby On Rails</a>
+            <br></br><br></br>
+            <a target="_blank" rel="noreferrer" href='https://developer.mozilla.org/en-US/docs/Web/JavaScript'>Javascript</a>
+            <br></br><br></br>
+            <a target="_blank" rel="noreferrer" href='https://reactjs.org/'>React</a>
+            <br></br><br></br>
+            <a target="_blank" rel="noreferrer" href='https://redux.js.org/'>Redux</a>
+            <br></br><br></br>
+            <a target="_blank" rel="noreferrer" href='https://www.python.org/doc/'>Python</a>
+            <br></br><br></br>
+        </div>
+    )
+}
